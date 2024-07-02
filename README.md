532d38486ba87d5df41de3d03b5e696043f0a491
Sabaa <you@example.com>
2024-07-03 00:17:33 +0300

    all

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## changes
+
+In this project I've modified the HTML and CSS.
+Also I've add a bunch of code using JS to do these things:
+- Adding the navbar list of links.
+- Scrolling will make the section active with the associated link in the navbar.
+- Go to the section when click on associated the link in navbar.
+- Hide fixed navigation bar while not scrolling (it should still be present on page load).
+- Add a scroll to the top button on the page that’s only visible when the user scrolls below the fold of the page.
+- Update/change the design/content **Some of them by JS**.
+- Make sections collapsible.
+- Make the landing page responsive.
+- ect.
+
+Note: I added `/* new */` on the style.css page to highlight added code by mine 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint quotes: ["error", "double"] */
 /**
  * 
  * Manipulating the DOM exercise.
@@ -22,26 +23,75 @@
  * Define Global Variables
  * 
 */
+let navbar__list = document.querySelector("#navbar__list");
+let page__header = document.querySelector(".page__header");
 
+let sections = document.querySelectorAll(".section");
+let content = document.querySelectorAll(".content");
+const showContent = document.querySelectorAll(".showContent");
+
+let goToTopBtn = document.querySelector("#goToTopBtn");
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+// add active class
 
+const addActiveClass = (section, toSection) => {
+    if (!section.classList.contains("your-active-class")) {
+        section.classList.add("your-active-class");
+    }
+    toSection.classList.add('bold');
+    toSection.style.borderBottom = "3px solid #cc1";
+}
+// remove active class
 
+const removeActiveClass = (sections, toSections) => {
+    for (let i = 0; i < toSections.length; i++) {
+        if (sections[i].classList.contains("your-active-class")) {
+            sections[i].classList.remove("your-active-class");
+        }
+        toSections[i].classList.remove('bold');
+        toSections[i].style.borderBottom = "0px ";
+    }
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+// collapsable sections
+for (let i = 0; i < showContent.length; i++) {
+
+    showContent[i].addEventListener('click', () => {
+        content[i].classList.toggle("content");
+    });
+
+}
 
 // build the nav
+const addLinksToNavbar = () => {
+    for (let i = 0; i < sections.length; i++) {
+        let li = document.createElement('li');
+        li.classList.add("toSection");
+        li.innerHTML =`<a href=" ">Section ${i + 1}</a>`;
+        navbar__list.appendChild(li);
 
+    }
+}
 
 // Add class 'active' to section when near top of viewport
+function makeActive(section, toSection) {
+    const box = section.getBoundingClientRect();
+    if (box.top <= 150 && box.bottom >= 150) {
+        addActiveClass(section, toSection);
+    } else {
+        removeActiveClass([section], [toSection]);
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -50,13 +100,66 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
+addLinksToNavbar();
 
-// Scroll to section on link click
+let toSections = document.querySelectorAll(".toSection");
 
-// Set sections as active
+for (let i = 0; i < toSections.length; i++) {
+    // Scroll to section on link click
+    toSections[i].addEventListener("click", (event) => {
+
+        event.preventDefault();
+        removeActiveClass(sections, toSections);
+        window.scrollTo(0, sections[i].offsetTop - 40);
+        // Set sections as active
+        addActiveClass(sections[i], toSections[i]);
+
+
+    });
+
+    document.addEventListener('scroll', () => {
+        page__header.style.display = "fixed";
+        // Set sections as active
+        makeActive(sections[i], toSections[i]);
+
+        // show the ToUp button
+        if (window.scrollY > 300) {
+            goToTopBtn.classList.add("showToUpBtn");
+        } else {
+            goToTopBtn.classList.remove("showToUpBtn");
+
+        }
+
+
+    });
+
+}
+
+// onscrollEnd hide the nav 
+document.onscrollend = () => {
+    setTimeout(() => {
+        page__header.style.display = "none";
+
+    }, 6 * 1000);
+
+}
+
+// onload display the nav 
+window.onload = () => {
+    page__header.style.display = "fixed";
+
+}
+
+
+
+
+
+
+
+
 
 
